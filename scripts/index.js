c596ffe123a3d7b13a0b4df799844891a8ac23bf
Vinogradov Dmitrij <dedok2242@yandex.ru>
2022-11-27 00:06:42 +0300

    tryv2

--- scripts/index.js
+++ scripts/index.js
@@ -2,35 +2,36 @@ const popupElement = document.querySelector('.popup');
 const popupCloseElement = popupElement.querySelector('.popup__close-icon');
 const popupOpenElement = document.querySelector('.profile__edit-button');
 const popupSubmitElement = popupElement.querySelector('.popup__submit');
-const likeElement = document.querySelector('.elements__like');
+//const likeElement = document.querySelector('.elements__like');
 const formPopup = document.querySelector('.popup__form');
 
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
-let inputProfileName = popupElement.querySelector('.form__input_type-name');
-let inputProfileStatus = popupElement.querySelector('.form__input_type-status'); 
+let inputProfileName = popupElement.querySelector('.form__input_type_name');
+let inputProfileStatus = popupElement.querySelector('.form__input_type_status'); 
 
-const toggleLikeElement = function(){
-   likeElement.classList.toggle('elements__like_active')
-}
-const deactivateLikeElement = function(){
-   likeElement.classList.remove('elements__like_active')
-}
+//const toggleLikeElement = function(){
+//   likeElement.classList.toggle('elements__like_active')
+//}
+//const deactivateLikeElement = function(){
+//   likeElement.classList.remove('elements__like_active')
+//}
 
 const openPopup = function(){
-  popupElement.classList.add('popup_is-opened');}
+  popupElement.classList.add('popup_opened');}
 
   const closePopup = function(){
-   popupElement.classList.remove('popup_is-opened');}
+   popupElement.classList.remove('popup_opened');}
 
 const renameingProfileandStatus = function(event){
-   profileName.textContent = `${inputProfileName.value}`;
-   profileStatus.textContent =`${inputProfileStatus.value}`;
+
+   profileName.textContent = inputProfileName.value;
+   profileStatus.textContent =inputProfileStatus.value;
    closePopup();
-   event.preventDefault();// Как я понял при сабмите дефолтное поведение-перезагрузка страницы,если убрать это,то value остается
+   event.preventDefault();
 }
 popupOpenElement.addEventListener('click', openPopup);
 popupCloseElement.addEventListener('click', closePopup);
-likeElement.addEventListener('click', toggleLikeElement);// Прочитал в слаке что это норма, что лайк нельзя поставить сразу на все картинки,но переменная связывается только с 1 элементом
+//likeElement.addEventListener('click', toggleLikeElement);// Прочитал в слаке что это норма, что лайк нельзя поставить сразу на все картинки,но переменная связывается только с 1 элементом
 formPopup.addEventListener('submit', renameingProfileandStatus);
